9256c7dd1894ff81ace4299e3e3af22b856a9c3d
gusgatam <gustavo.gatamora@gmail.com>
2020-12-04 14:59:24 -0300

    Devolcuión.
    Comentarios en el primer ejercicio, subir trabajos adeudados.

--- vectores/1.cpp
+++ vectores/1.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int y,rt=0,r[]{1,2,3,4,5};
-for (y=0;y<5;y++){
+int main()//Faltan las funciones.
+{
+int y,rt=0,r[5]{0};//Se inicializa el vector en 0.
+for (y=0;y<5;y++)
+{
 printf("Ingrese las resistencia ");
-scanf("%d",&r[y]);}
-for (y=0;y<5;y++){
+scanf("%d",&r[y]);
+}
+for (y=0;y<5;y++)
+{
 printf("\nlas resistencias son:%d",r[y]);
 rt=rt+r[y];
 }
